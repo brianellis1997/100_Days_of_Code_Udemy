9e5b2d9baddd9bbed631211655ce76723a8d8758
Brian Ellis <brianellis1997@gmail.com>
2024-07-27 17:57:08 -0400

    day 15 init2

---
+++ Days/Day_15.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 1. Print report of all coffee machine resources.
+def coffee_machine():
+    user_input = input('What would you like? (espresso/latte/capuccino): ').lower()
