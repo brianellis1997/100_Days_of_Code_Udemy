1f2ba7f9501a63c2112ebcf72612d591d79c9efa
Brian Ellis <brianellis1997@gmail.com>
2024-07-27 18:28:40 -0400

    Working on check_payment function

--- Days/Day_15.py
+++ Days/Day_15.py
@@ -34,5 +34,41 @@ resources = {
 
 
 # TODO: 1. Print report of all coffee machine resources.
+
+def check_resources(drink, resources):
+    if drink[ingredients][water] > resources[water]:
+        print("Sorry there is not enough water.")
+        return
+    elif drink[ingredients][milk] > resources[milk]:
+        print("Sorry there is not enough milk.")
+        return
+    elif drink[ingredients][coffee] > resources[coffee]:
+        print("Sorry there is not enough coffee.")
+        return
+
+def check_payment(quarters, dimes, nickles, pennies, money, cost, drink, emoji):
+    payment = (quarters*25 + dimes*10 + nickles*5 + pennies) / 100
+    if payment < cost:
+        print("Sorry that's not enough money. Money refunded")
+        return
+    change = payment - cost
+    money += cost
+    if change > 0:
+        print(f"Here is ${change} in change.")
+    print(f"Here is your {drink}  Enjoy!") # Enter emoji from keyboard
+
 def coffee_machine():
-    user_input = input('What would you like? (espresso/latte/capuccino): ').lower()
+    money = 0
+    user_prompt = input("What would you like? (espresso/latte/capuccino): ")
+    if user_prompt[0] == 'r':
+        print(f"""
+        Water:  {resources[water]}ml
+        Milk:   {resources[milk]}ml
+        Coffee: {resouces[coffee]}g
+        Money:  ${money}""")
+
+
+
+
+
+
