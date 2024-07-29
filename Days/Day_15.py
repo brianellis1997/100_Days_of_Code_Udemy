65682d2a756978106d9d1f94fd7479ce98e3e342
Brian Ellis <brianellis1997@gmail.com>
2024-07-29 16:21:29 -0400

    updated functions for better module handling.

--- Days/Day_15.py
+++ Days/Day_15.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -32,43 +30,69 @@ resources = {
     "coffee": 100,
 }
 
+def check_resources(drink, items):
+    for ingredient in MENU[drink]['ingredients']:
+        if MENU[drink]['ingredients'][ingredient] > items.get(ingredient, 0):
+            print(f"Sorry there is not enough {ingredient}.")
+            return False
+    return True
 
-# TODO: 1. Print report of all coffee machine resources.
-
-def check_resources(drink, resources):
-    if drink[ingredients][water] > resources[water]:
-        print("Sorry there is not enough water.")
-        return
-    elif drink[ingredients][milk] > resources[milk]:
-        print("Sorry there is not enough milk.")
-        return
-    elif drink[ingredients][coffee] > resources[coffee]:
-        print("Sorry there is not enough coffee.")
-        return
-
-def check_payment(quarters, dimes, nickles, pennies, money, cost, drink, emoji):
-    payment = (quarters*25 + dimes*10 + nickles*5 + pennies) / 100
-    if payment < cost:
-        print("Sorry that's not enough money. Money refunded")
-        return
-    change = payment - cost
+def update_money(money, cost):
     money += cost
+    return money
+
+def check_payment(quarters, dimes, nickels, pennies, money, cost, drink):
+    payment = (quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01)
+    if payment < cost:
+        print("Sorry that's not enough money. Money refunded.")
+        return False, money
+
+    money = update_money(money, cost)
+
+    change = payment - cost
     if change > 0:
-        print(f"Here is ${change} in change.")
-    print(f"Here is your {drink}  Enjoy!") # Enter emoji from keyboard
+        print(f"Here is ${round(change, 2)} in change.")
+    print(f"Here is your {drink} ☕️ Enjoy!")
+    return True, money
+
+def get_cost(drink):
+    return MENU[drink]['cost']
+
+def sale(selection, money, resources):
+    if not check_resources(selection, resources):
+        return money
+    print("Please insert coins.")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickels = int(input("How many nickels?: "))
+    pennies = int(input("How many pennies?: "))
+    successful_payment, money = check_payment(quarters, dimes, nickels, pennies, money, get_cost(selection), selection)
+    if successful_payment:
+        resources = update_resources(resources, selection)
+    return money
+
+def update_resources(resources, selection):
+    for ingredient in MENU[selection]['ingredients']:
+        resources[ingredient] -= MENU[selection]['ingredients'][ingredient]
+    return resources
 
 def coffee_machine():
     money = 0
-    user_prompt = input("What would you like? (espresso/latte/capuccino): ")
-    if user_prompt[0] == 'r':
-        print(f"""
-        Water:  {resources[water]}ml
-        Milk:   {resources[milk]}ml
-        Coffee: {resouces[coffee]}g
-        Money:  ${money}""")
+    power = True
+    while power:
+        user_prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if user_prompt == 'report':
+            print(f"""
+            Water:  {resources['water']}ml
+            Milk:   {resources['milk']}ml
+            Coffee: {resources['coffee']}g
+            Money:  ${money}""")
+        elif user_prompt in MENU:
+            money = sale(user_prompt, money, resources)
+        else:
+            print("Sorry, you didn't enter a proper selection.")
+        cont = input("Would you like something else? (yes/no): ").lower()
+        power = False if cont == 'no' else True
+    print("We're closed. Thanks for stopping by!")
 
-
-
-
-
-
+coffee_machine()
